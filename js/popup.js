e380d9c75b5cb1b2fddedcaf037dbb6653174c2c
vineetbhoriya <105233380+vineetbhoriya@users.noreply.github.com>
2022-12-16 19:36:00 +0530

    this is new project
    just for practice

---
+++ js/popup.js
@@ -0,0 +1,6 @@
+function my_popup() {
+    document.getElementById("popup_main-box").style.display = "block";
+}
+function my_popup_close() {
+    document.getElementById("popup_main-box").style.display = "none";
+}
